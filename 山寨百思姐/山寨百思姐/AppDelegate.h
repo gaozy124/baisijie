8be2419a5999148588da72aca0d0bb5414d8b4cd
gzy <298012895@qq.com>
2016-02-09 14:08:43 +0800

    基本配置

---
+++ 山寨百思姐/山寨百思姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  山寨百思姐
+//
+//  Created by 高志宇 on 16/2/9.
+//  Copyright © 2016年 高志宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 山寨百思姐/山寨百思姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  山寨百思姐
+//
+//  Created by 高志宇 on 16/2/9.
+//  Copyright © 2016年 高志宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
